4b9434c42b74c641dd4801a3d11d2cdc36732469
silvhua <silvhua@gmail.com>
2024-06-04 11:08:25 -0700

    add event handler to <form> isntead of <button>

--- 06-react/lab-react-props-starter/src/components/CardForm.jsx
+++ 06-react/lab-react-props-starter/src/components/CardForm.jsx
@@ -1,15 +1,15 @@
 const CardForm = () => {
     const submitHandler = (event) => {
         event.preventDefault();
-        console.log(`Title: ${event.target.parentNode.title.value}\nContent: ${event.target.parentNode.content.value}`);
+        console.log(`Title: ${event.target.title.value}\nContent: ${event.target.content.value}`);
     }
     return (
-        <form>
+        <form onSubmit={submitHandler}>
             <label htmlFor="title">Title</label>
             <input name="title" id="title"/>
             <label htmlFor="content">Content</label>
             <input name="content" id="content"/>
-            <button onClick={submitHandler}>Submit</button>
+            <button>Submit</button>
         </form>
     )
 }
