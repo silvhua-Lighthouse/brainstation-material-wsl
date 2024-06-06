179218fb8b5e0a194edf1b2b3cabb342a396f9c9
silvhua <silvhua@gmail.com>
2024-06-06 15:10:37 -0700

    2024-06-06

--- 07-spa/starter-dynamic-urls-with-react-router/src/pages/ProductDetailsPage/ProductDetailsPage.jsx
+++ 07-spa/starter-dynamic-urls-with-react-router/src/pages/ProductDetailsPage/ProductDetailsPage.jsx
@@ -1,8 +1,21 @@
 // import './ProductDetailsPage.scss';
+import products from '../../data/products.json'
+import { Navigate, useParams } from 'react-router-dom';
 
 const ProductDetailsPage = () => {
+    const paramHook = useParams();
+    const {productId} = useParams();
+    const product = products.find(product => product.id === productId);
+    if (!product) {
+        return <Navigate to="/products"></Navigate>
+    }
+
     return (
         <>
+        <h1>{product.name} {product.icon}</h1>
+        <p>{product.description}</p>
+        <p>Product ID: {product.id}</p>
+        <p>{product.technicalSpecs}</p>
         </>
     )
 }
--- 07-spa/starter-dynamic-urls-with-react-router/src/pages/ProductsPage/ProductsPage.jsx
+++ 07-spa/starter-dynamic-urls-with-react-router/src/pages/ProductsPage/ProductsPage.jsx
@@ -1,12 +1,22 @@
 // import './ProductsPage.scss';
+import products from '../../data/products.json'
 
 const ProductsPage = () => {
-
-    return (
-        <>
-        <h1>Products Page</h1>
-        </>
-    )
+  
+  return (
+    <>
+    <h1>Products Page</h1>
+    {
+      products.map( product => {
+        return (
+          <li key={product.id}>
+            {product.name}
+          </li>
+        )
+      })
+    }
+    </>
+  )
 }
 
 export default ProductsPage
